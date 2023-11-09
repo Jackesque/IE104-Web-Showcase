a18208de35f6066543347630bdcff8d58d227ff3
jackesque <127185476+Jackesque@users.noreply.github.com>
2023-11-09 23:15:35 +0700

    add ajax to page

---
+++ W3/14 - SCAALCoffee/index.js
@@ -0,0 +1,10 @@
+const AJAXReload = (href) => {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      noidung.innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", href, false);
+  xhttp.send();
+}

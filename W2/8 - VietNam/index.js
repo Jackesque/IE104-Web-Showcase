1d0cdced69debcef3466e4587860a98f7bcbd550
jackesque <127185476+Jackesque@users.noreply.github.com>
2023-11-09 22:15:57 +0700

    fix audio

--- W2/8 - VietNam/index.js
+++ W2/8 - VietNam/index.js
@@ -4,6 +4,6 @@ pageAudio.play();
 
 const loadHandler = (id) => {
   const ele = document.getElementById(id);
-  ele.before((ele.contentDocument.body.children[0] || ele.contentDocument));
+  ele.before(ele.contentDocument.body.children[0] || ele.contentDocument);
   ele.remove();
 }
